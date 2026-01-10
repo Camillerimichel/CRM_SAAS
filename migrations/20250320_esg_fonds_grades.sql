c9ce9039676641797c5347e685d7bad804ddcda5
root <root@srv1114982.hstgr.cloud>
2026-01-10 15:05:56 +0000

    Add ESG grade recompute and admin action

---
+++ migrations/20250320_esg_fonds_grades.sql
@@ -0,0 +1,5 @@
+ALTER TABLE esg_fonds
+  ADD COLUMN note_e_grade VARCHAR(2) NULL,
+  ADD COLUMN note_s_grade VARCHAR(2) NULL,
+  ADD COLUMN note_g_grade VARCHAR(2) NULL,
+  ADD COLUMN note_esg_grade VARCHAR(2) NULL;
